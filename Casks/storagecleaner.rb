0853b1b5445e5456497b0cee0f846fffb4da5f29
snvladm <8c8v@mail.ru>
2026-09-21 22:54:04 +0300

    storagecleaner 1.0.0

--- Casks/storagecleaner.rb
+++ Casks/storagecleaner.rb
@@ -1,6 +1,6 @@
 cask "storagecleaner" do
   version "1.0.0"
-  sha256 "a77d4646cd72bee416b94c21216b95684153d73b65a64316be71862bb3108009"
+  sha256 "4ebdab096e09dd476a4d4407f558c25a8dd66cdb020b40dd1f8304324a26aae7"
 
   url "https://github.com/snvladm/storagecleaner-releases/releases/download/v#{version}/StorageCleaner-#{version}.dmg"
   name "StorageCleaner"
